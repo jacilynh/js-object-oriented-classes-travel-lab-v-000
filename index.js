bf2ab9ff85ab0b6b463050549cfcc6e9a9dea213
Jacilyn Hayden <jacihayden@gmail.com>
2019-01-30 02:49:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-
+// **Driver**
+// - A driver can be initialized with a name, and a string representing the day
+//   that he joined Scuber. 
+// * `startDate` — returns a JavaScript Date object. 
+// * `yearsExperienceFromBeginningOf` — takes an argument of a year and returns the
+//   number of years since the driver's startDate. 
+  
+  class Driver {
+    constructor(name, startDate) {
+      this.name = name;
+      this.startDate = new Date(startDate);
+    }
+    
+    yearsExperienceFromBeginningOf(year) {
+      let yr = new Date(year, 1, 1);
+      let 
+    }
+  }
